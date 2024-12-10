53198f85a9f43fe4a2efadcb8e9c3b53d178bf55
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-12-10 03:34:28 -0500

    Updating: Cart structure added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
   },
 ];
