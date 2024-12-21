6cfbb77701c5e3da0440b9bcffae2253ebfeb849
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2024-12-21 06:33:20 -0500

    Adding: pagination for products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,23 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { ProductsLayoutComponent } from './components/products-layout/products-layout.component';
 import { FiltersModalComponent } from './components/filters-modal/filters-modal.component';
 import { ProductService } from '../core/services/product.service';
 import { ProductCardComponent } from '../shared/components/product-card/product-card.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AsyncPipe, TitleCasePipe } from '@angular/common';
-import { ProductCategory } from '../shared/models/product';
+import { Product, ProductCategory } from '../shared/models/product';
 import { FormatCategoryPipe } from '../shared/pipes/format-category.pipe';
+import { map, Observable } from 'rxjs';
+import { getProductPages, MAX_PRODUCTS_PER_PAGE } from '../utils/pagination';
 
 @Component({
   selector: 'app-products',
@@ -21,10 +31,10 @@ import { FormatCategoryPipe } from '../shared/pipes/format-category.pipe';
     AsyncPipe,
   ],
   templateUrl: './products.component.html',
-  styleUrls: ['../shared/styles/pagination.css'],
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnChanges {
   productService = inject(ProductService);
+  router = inject(Router);
 
   rating = input<string>();
   minPrice = input<string>();
@@ -40,6 +50,9 @@ export class ProductsComponent implements OnInit {
     this.maxPrice() ? Number(this.maxPrice()) : 3500
   );
   selectedCategory = computed(() => this.category());
+  selectedPage = computed(() =>
+    this.page() ? parseInt(this.page() as string) : 1
+  );
 
   products = computed(() =>
     this.productService.getByFilters({
@@ -49,10 +62,59 @@ export class ProductsComponent implements OnInit {
       maxPrice: this.selectedMaxPrice(),
     })
   );
+  productPages = computed(() =>
+    this.products().pipe(map((products) => getProductPages(products)))
+  );
+  pageProducts!: Observable<Product[]>;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { page } = changes;
+
+    if (page) {
+      this.pageProducts = this.getPageProducts();
+    }
+  }
 
   ngOnInit(): void {
+    this.pageProducts = this.getPageProducts();
+
     setTimeout(() => {
       initFlowbite();
     }, 100);
+  }
+
+  private getPageProducts() {
+    return this.products().pipe(
+      map((products) => {
+        const currentPage = this.page() ? parseInt(this.page() as string) : 1;
+        const lastIndex = currentPage * MAX_PRODUCTS_PER_PAGE;
+        const initialIndex = lastIndex - MAX_PRODUCTS_PER_PAGE;
+        const pageProducts = [];
+
+        for (let i = 0; i < MAX_PRODUCTS_PER_PAGE; i++) {
+          const product = products[i + initialIndex];
+
+          if (product) pageProducts.push(product);
+        }
+
+        return pageProducts;
+      })
+    );
+  }
+
+  loadPage(page: number) {
+    this.productPages().subscribe((pages) => {
+      if (!pages.includes(page)) return;
+
+      this.router.navigate(['/products'], {
+        queryParams: {
+          category: this.selectedCategory(),
+          rating: this.selectedRating(),
+          minPrice: this.selectedMinPrice(),
+          maxPrice: this.selectedMaxPrice(),
+          page,
+        },
+      });
+    });
   }
 }
